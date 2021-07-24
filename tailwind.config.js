b0177046b710301b6ae45520f2ca236157a95950
safecorners <safeconers.mobile@gmail.com>
2021-07-25 04:16:29 +0900

    add colors.teal

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      teal: colors.teal
+    },
     extend: {},
   },
   variants: {
